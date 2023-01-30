70d44c83cfcc7172855f66fda558c974cfde8f91
mika <mika.stamm1@gmail.com>
2023-01-30 20:25:07 +0000

    Add more scritps

--- deploy/database.bash
+++ deploy/database.bash
@@ -1,2 +1,11 @@
 #!/bin/bash
 # Deploys the database to the prod environment
+
+#Remove dev user
+echo "🗑️ Removing Dev User"
+wp user delete dev --yes
+
+echo "🔍 Replacing site url"
+wp search-replace $devSiteUrl $prodSiteUrl
+
+echo "Creating snapshot of prod database"
